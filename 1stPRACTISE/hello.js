6057e2dd80be08e060d7a94f071f8ad7a3a01683
Swarup Mitra <swarupmtra3456@gmail.com>
2025-07-10 01:44:58 +0530

    new changes

--- 1stPRACTISE/hello.js
+++ 1stPRACTISE/hello.js
@@ -1,2 +1,10 @@
-console.log("Hello, World!");
-console.log("This is my first JavaScript file.");
+const http = require('http');
+const server = http.createServer((req, res) => {
+  if (req.url==='/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    
+  }
+});
+server.listen(3000, () => {
+  console.log('Server running at http://localhost:3000/');
+});
