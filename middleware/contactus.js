08ce7b0295dbb5999844d5741744839ff24154d0
Swarup Mitra <swarupmtra3456@gmail.com>
2025-07-17 01:46:25 +0530

    new changes done

--- middleware/contactus.js
+++ middleware/contactus.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const app = express();
+const dummymmiddleware = (req, res, next) => {
+  console.log(req.path, req.method);
+  next();
+};
+app.use(dummymmiddleware);
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000');
 });
